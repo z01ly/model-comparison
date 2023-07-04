18e206c819ef010297871349dd3bdeae1344a0b1
z01ly <lingyzh@outlook.com>
2023-07-04 14:55:55 +0200

    update mmdVAE

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 def sdss_size():
-    filepath = "../cutouts_1000/3000.png"
+    filepath = "../cutouts_1000/cutouts_1000_train/3000.png"
     img = Image.open(filepath)
 
     print("{} x {}".format(img.height, img.width))
@@ -22,8 +22,15 @@ def sdss1000_load(path):
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=20, shuffle=True)
 
     return dataloader
+# train_data = sdss1000_load('../cutouts_1000')
+# test_data = sdss1000_load('../cutouts_1000_2')
 
-train_data = sdss1000_load('../cutouts_1000')
-test_data = sdss1000_load('../cutouts_1000_2')
+def conv_size_comp(img_size):
+    F = 4 # filter size
+    P = 1 # padding
+    S = 2 # stride
+    W = (img_size - F + 2 * P) / S + 1
 
-
+    return int((W - F + 2 * P) / S + 1)
+# print(conv_size_comp(28))
+# print(conv_size_comp(64))
