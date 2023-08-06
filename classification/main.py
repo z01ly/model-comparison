ee8695fbf404672c3b296081537664e95b4745c7
z01ly <lingyzh@outlook.com>
2023-08-06 10:28:33 +0200

    checkpoint

--- classification/main.py
+++ classification/main.py
@@ -5,26 +5,32 @@ import matplotlib.pyplot as plt
 import math
 import os
 
-from sklearn.preprocessing import LabelBinarizer
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.linear_model import LogisticRegression
 # from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from imblearn.ensemble import BalancedRandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import BernoulliNB
+
+from xgboost import XGBClassifier
 
 
-def classification(key):
+def classification(encoder_key, classifier_key):
     AGN_data = np.load('../infoVAE/test_results/latent/AGN.npy')
     AGN_labels = np.full(AGN_data.shape[0], 'AGN') 
 
     NOAGN_data = np.load('../infoVAE/test_results/latent/NOAGN.npy')
     NOAGN_labels = np.full(NOAGN_data.shape[0], 'NOAGN') 
 
-    UHD_data = np.load('../infoVAE/test_results/latent/UHD.npy')
-    UHD_labels = np.full(UHD_data.shape[0], 'UHD') 
-
     n80_data = np.load('../infoVAE/test_results/latent/n80.npy')
     n80_labels = np.full(n80_data.shape[0], 'n80') 
+
+    UHD_data = np.load('../infoVAE/test_results/latent/UHD.npy')
+    UHD_labels = np.full(UHD_data.shape[0], 'UHD') 
 
     X = np.concatenate((AGN_data, NOAGN_data, n80_data, UHD_data), axis=0)
     y = np.concatenate((AGN_labels, NOAGN_labels, n80_labels, UHD_labels), axis=0)
@@ -33,14 +39,37 @@ def classification(key):
     X = X[random_indices]
     y = y[random_indices]
 
-    label_binarizer = LabelBinarizer()
-    y_onehot = label_binarizer.fit_transform(y)
+
+    if encoder_key == 'one-hot':
+        label_binarizer = LabelBinarizer()
+        y_onehot = label_binarizer.fit_transform(y)
+    elif encoder_key == 'integer':
+        label_binarizer = LabelEncoder()
+        y_onehot = label_binarizer.fit_transform(y)
 
     for class_label, onehot_vector in zip(label_binarizer.classes_, label_binarizer.transform(label_binarizer.classes_)):
-        print(f"Class '{class_label}' is transformed to one-hot encoding vector: {onehot_vector}")
+        print(f"Class '{class_label}' is transformed to encoding vector: {onehot_vector}")
 
-    # later maybe: BalancedRandomForestClassifier from imbalanced-learn lib
-    rf_classifier = RandomForestClassifier(n_estimators=10, random_state=42, class_weight='balanced_subsample') # class weight for imbalanced data
+
+    if classifier_key == 'random-forest':
+        clf = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced_subsample')
+    elif classifier_key == 'balanced-random-forest':
+        clf = BalancedRandomForestClassifier(n_estimators=100, random_state=42, \
+                sampling_strategy='all', replacement=True)
+    elif classifier_key == 'xgboost':
+        clf = XGBClassifier()
+    elif classifier_key == 'logistic-regression':
+        clf = LogisticRegression(multi_class='multinomial', class_weight='balanced', max_iter=500, solver='lbfgs')
+    elif classifier_key == 'gradient-boosting':
+        clf = GradientBoostingClassifier(n_estimators=100, random_state=42)
+    elif classifier_key == 'svc':
+        clf = SVC(kernel='rbf', class_weight='balanced', random_state=42, decision_function_shape='ovr')
+    elif classifier_key == 'knn':
+        clf = KNeighborsClassifier(n_neighbors=1, weights='uniform', algorithm='kd_tree')
+    elif classifier_key == 'naive-bayes':
+        clf = BernoulliNB()
+
+    
 
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
     kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
@@ -54,9 +83,9 @@ def classification(key):
         y_train_onehot = label_binarizer.transform(y_train)
         y_test_onehot = label_binarizer.transform(y_test)
 
-        rf_classifier.fit(X_train, y_train_onehot)
+        clf.fit(X_train, y_train_onehot)
 
-        y_pred_onehot = rf_classifier.predict(X_test)
+        y_pred_onehot = clf.predict(X_test)
 
         y_pred = label_binarizer.inverse_transform(y_pred_onehot)
         
@@ -70,30 +99,14 @@ def classification(key):
 
     fig, ax = plt.subplots(figsize=(8, 6))
     disp.plot(cmap='Blues', ax=ax, values_format='d')
-    plt.title('Confusion Matrix')
-    plt.savefig('./random_forest_cm.png')
 
-"""
-    plt.imshow(sum_cm, cmap='Blues')
-
-    num_classes = len(label_binarizer.classes_)
-    tick_marks = np.arange(num_classes)
-    plt.xticks(tick_marks, label_binarizer.classes_)
-    plt.yticks(tick_marks, label_binarizer.classes_)
-    plt.xlabel('Predicted Class')
-    plt.ylabel('True Class')
-    plt.title('Confusion Matrix')
-
-    for i in range(num_classes):
-        for j in range(num_classes):
-            plt.text(j, i, str(sum_cm[i, j]), horizontalalignment="center", color="black")
-
-    plt.colorbar()
-    plt.savefig('./random_forest_cm.png')
-"""
+    plt.title(classifier_key + ' Confusion Matrix')
+    plt.savefig('./confusion-matrix/' + classifier_key + '-cm.png')
 
 
 
 
 if __name__ == "__main__":
-    classification()
+    # imbalanced data
+    classification('integer', 'naive-bayes')
+    
