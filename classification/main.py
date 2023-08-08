61ba5583aad2f458903fbefb0def160cbc2df627
z01ly <lingyzh@outlook.com>
2023-08-08 17:08:18 +0200

    confusion matrix and calibration curve

--- classification/main.py
+++ classification/main.py
@@ -9,7 +9,7 @@ from sklearn.preprocessing import LabelBinarizer, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.linear_model import LogisticRegression
 # from sklearn.model_selection import train_test_split
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import StratifiedKFold, RepeatedStratifiedKFold
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 from imblearn.ensemble import BalancedRandomForestClassifier
 from sklearn.svm import SVC
@@ -17,6 +17,8 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.naive_bayes import BernoulliNB
 
 from xgboost import XGBClassifier
+
+from sklearn.calibration import calibration_curve
 
 
 def classification(encoder_key, classifier_key):
@@ -26,14 +28,14 @@ def classification(encoder_key, classifier_key):
     NOAGN_data = np.load('../infoVAE/test_results/latent/NOAGN.npy')
     NOAGN_labels = np.full(NOAGN_data.shape[0], 'NOAGN') 
 
-    n80_data = np.load('../infoVAE/test_results/latent/n80.npy')
-    n80_labels = np.full(n80_data.shape[0], 'n80') 
-
     UHD_data = np.load('../infoVAE/test_results/latent/UHD.npy')
     UHD_labels = np.full(UHD_data.shape[0], 'UHD') 
 
-    X = np.concatenate((AGN_data, NOAGN_data, n80_data, UHD_data), axis=0)
-    y = np.concatenate((AGN_labels, NOAGN_labels, n80_labels, UHD_labels), axis=0)
+    n80_data = np.load('../infoVAE/test_results/latent/n80.npy')
+    n80_labels = np.full(n80_data.shape[0], 'n80') 
+
+    X = np.concatenate((AGN_data, NOAGN_data, UHD_data, n80_data), axis=0)
+    y = np.concatenate((AGN_labels, NOAGN_labels, UHD_labels, n80_labels), axis=0)
 
     random_indices = np.random.permutation(len(X)) # data shuffle
     X = X[random_indices]
@@ -57,24 +59,29 @@ def classification(encoder_key, classifier_key):
         clf = BalancedRandomForestClassifier(n_estimators=100, random_state=42, \
                 sampling_strategy='all', replacement=True)
     elif classifier_key == 'xgboost':
-        clf = XGBClassifier()
+        clf = XGBClassifier(objective='multi:softmax')
     elif classifier_key == 'logistic-regression':
         clf = LogisticRegression(multi_class='multinomial', class_weight='balanced', max_iter=500, solver='lbfgs')
     elif classifier_key == 'gradient-boosting':
         clf = GradientBoostingClassifier(n_estimators=100, random_state=42)
     elif classifier_key == 'svc':
-        clf = SVC(kernel='rbf', class_weight='balanced', random_state=42, decision_function_shape='ovr')
+        clf = SVC(kernel='rbf', class_weight='balanced', random_state=42, decision_function_shape='ovr', probability=True)
     elif classifier_key == 'knn':
-        clf = KNeighborsClassifier(n_neighbors=1, weights='uniform', algorithm='kd_tree')
+        clf = KNeighborsClassifier(n_neighbors=3, weights='uniform', algorithm='kd_tree')
     elif classifier_key == 'naive-bayes':
         clf = BernoulliNB()
 
     
-
     # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42, stratify=y)
-    kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+    # kf = StratifiedKFold(n_splits=5, shuffle=True, random_state=0)
+    n_splits = 5
+    n_repeats = 5
+    kf = RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=n_repeats, random_state=0)
 
     confusion_matrices = []
+
+    all_probabilities = []
+    all_true_labels = []
 
     for train_index, test_index in kf.split(X, y):
         X_train, X_test = X[train_index], X[test_index]
@@ -82,18 +89,41 @@ def classification(encoder_key, classifier_key):
 
         y_train_onehot = label_binarizer.transform(y_train)
         y_test_onehot = label_binarizer.transform(y_test)
-
+    
         clf.fit(X_train, y_train_onehot)
-
+    
         y_pred_onehot = clf.predict(X_test)
-
         y_pred = label_binarizer.inverse_transform(y_pred_onehot)
-        
         cm = confusion_matrix(y_test, y_pred)
         confusion_matrices.append(cm)
 
+        fold_probabilities = clf.predict_proba(X_test)
+        all_probabilities.append(fold_probabilities)
+        all_true_labels.append(y_test)
+
+
+    probabilities = np.concatenate(all_probabilities, axis=0)
+    true_labels = np.concatenate(all_true_labels, axis=0)
+
+    plt.figure(figsize=(10, 10))
+    markers = ['o', 'v', 's', 'p']
+    linestyles = ['solid', 'dotted', 'dashed', 'dashdot']
+    # for model_idx, model_label in enumerate(['AGN', 'NOAGN', 'UHD', 'n80']):
+    for model_idx, model_label in enumerate(label_binarizer.inverse_transform(clf.classes_)):
+        prob_true, prob_pred = calibration_curve(true_labels == model_label, probabilities[:, model_idx], n_bins=10)
+        plt.plot(prob_pred, prob_true, marker=markers[model_idx], linestyle=linestyles[model_idx], label=f'{model_label}')
+
+    plt.plot([0, 1], [0, 1], linestyle='--', color='gray', label='Ideal')
+    plt.xlabel('Predicted Probability')
+    plt.ylabel('Empirical Probability')
+    plt.title('Calibration Curve: ' + classifier_key)
+    plt.legend()
+    plt.savefig('./calibration-curve/' + classifier_key + '-cc.png')
+    plt.close()
+
+
     # average_cm = np.mean(confusion_matrices, axis=0)
-    sum_cm = np.sum(confusion_matrices, axis=0)
+    sum_cm = np.int64(np.sum(confusion_matrices, axis=0) / n_repeats)
 
     disp = ConfusionMatrixDisplay(confusion_matrix=sum_cm, display_labels=label_binarizer.classes_)
 
@@ -102,11 +132,20 @@ def classification(encoder_key, classifier_key):
 
     plt.title(classifier_key + ' Confusion Matrix')
     plt.savefig('./confusion-matrix/' + classifier_key + '-cm.png')
+    plt.close()
 
+    
 
 
 
 if __name__ == "__main__":
     # imbalanced data
+    # classification('integer', 'random-forest')
+    # classification('integer', 'balanced-random-forest')
+    # classification('integer', 'xgboost')
+    # classification('integer', 'logistic-regression')
+    # classification('integer', 'gradient-boosting')
+    # classification('integer', 'svc')
+    # classification('integer', 'knn')
     classification('integer', 'naive-bayes')
     
