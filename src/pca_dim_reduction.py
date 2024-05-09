b8370c1ce856bfc7060228099d25a3b319fbb929
z01ly <lingyzh@outlook.com>
2024-05-09 18:00:05 +0200

    update

--- src/pca_dim_reduction.py
+++ src/pca_dim_reduction.py
@@ -10,13 +10,14 @@ from sklearn.preprocessing import StandardScaler
 
 
 
-def lower_dim(z_dim):
-    sdss_test_df = pd.read_pickle(os.path.join('src/results/latent-vectors', 'sdss', 'test.pkl'))
+def lower_dim(savepath_prefix, z_dim):
+    os.makedirs(os.path.join(savepath_prefix, 'pca'), exist_ok=True)
+
+    sdss_test_df = pd.read_pickle(os.path.join(savepath_prefix, 'latent-vectors', 'sdss', 'test.pkl')) 
     sdss_test_data = sdss_test_df.iloc[:, 0:z_dim].to_numpy()
     sc = StandardScaler()
     sdss_test_data = sc.fit_transform(sdss_test_data)
 
-    
     dim_list = []
     sum_explained_var_ratio_list = []
     for n_components in range(1, z_dim+1):
@@ -28,7 +29,7 @@ def lower_dim(z_dim):
 
     fig = plt.figure(figsize=(12, 6))
     plt.plot(dim_list, sum_explained_var_ratio_list)
-    plt.xticks(range(1, 36))
+    plt.xticks(range(1, z_dim+2))
     plt.yticks([i/10 for i in range(11)])
     plt.ylabel('Sum of Explained variance Ratio')
     plt.xlabel('Number of Principal components')
@@ -36,9 +37,12 @@ def lower_dim(z_dim):
     plt.grid(True)
     # plt.legend(loc='best')
     plt.tight_layout()
-    fig.savefig('src/pca/results/explained_var_ratio.png', bbox_inches='tight')
+    fig.savefig(os.path.join(savepath_prefix, 'pca', 'explained_var_ratio.png'), bbox_inches='tight')
+    
 
 
 
 if __name__ == "__main__":
-    lower_dim(32)
+    nz = 32
+    savepath_prefix = 'results/' + str(nz) + '-dims'
+    lower_dim(savepath_prefix, nz)
